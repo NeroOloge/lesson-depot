a6b1da83c7b2e60aa7ea33e687836d21ed752561
Oghenero Ologe <nologe37@gmail.com>
2024-10-09 10:43:01 +0100

    installed tailwind

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-import './assets/main.css'
+// import './assets/main.css'
+import "./style.css";
 
 new Vue({
-  render: (h) => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
